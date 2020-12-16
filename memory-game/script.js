5f41deb41ad648b4196dee85913649ab82f0b623
Vladimir <vladimir.selevertov@gmail.com>
2020-12-16 23:57:13 +0200

    remove path

--- memory-game/script.js
+++ memory-game/script.js
@@ -70,7 +70,7 @@ document.addEventListener("DOMContentLoaded", () => {
       		<img class="front-img" src="${pathToStockImg}">
       	  </div>
       	  <div class="flip-card-back">
-      		<img class="back-img" src="${cardArray[i].img}">
+      		<img class="back-img" src="./img/empty-block.png">
       	  </div>
       	</div>`;
 
